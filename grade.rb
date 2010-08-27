066a55551346685ed91b130c9d59382570406826
Chris Apolzon <apolzon@gmail.com>
2010-08-27 15:30:42 -0700

    implemented comparable

---
+++ grade.rb
@@ -0,0 +1,25 @@
+class Grade
+	include Comparable
+	attr_accessor :letter, :modifier
+	def initialize(value)
+		# separate letter from identifier
+		@letter = value[0].chr
+		unless value[1].nil?
+			@modifier = (value[1].chr == "+") ? 1 : -1
+		else
+			@modifier = 0
+		end
+	end
+
+	def <=>(other)
+		# Return -1 if self is < other
+		# Return 0 if self == other
+		# Return +1 if self > other
+		if @letter != other.letter
+			# We want to flip the polarity of this one (a comes before b, but we want it to be a > b)
+			# So we flip the side that self and input are on
+			return other.letter <=> @letter
+		end
+		@modifier <=> other.modifier
+	end
+end
